8bfdfe1255e7d5ff035f93f7f2fc9b5a116d0699
tonyLarkVM <anthonylark@arizona.edu>
2024-05-10 00:13:03 -0700

    Completed tutorial 3

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,9 @@
 
+#include "types.h"
+
 void printf(char* str)
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; ++i){
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -18,7 +20,7 @@ extern "C" void callConstructors(){ //called in loader.s
         (*i)();
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t)
 {
     printf("Hello World!");
 
