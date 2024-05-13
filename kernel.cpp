4d9d8815713aace349991288a06dad9dced43f3e
tonyLarkVM <anthonylark@arizona.edu>
2024-05-12 22:07:12 -0700

    improved the functionality of kernel.cpp it now responds to \n and can printf multiple times!

--- kernel.cpp
+++ kernel.cpp
@@ -6,8 +6,36 @@ void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
+    static uint8_t x=0 , y=0;
+
     for(int i = 0; str[i] != '\0'; ++i){
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+        switch(str[i]){
+            case '\n': // new line protocal for \n
+                y++;
+                x=0;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++; //80 is the width of the screen 25 is the length
+                break;
+        }
+
+        if (x >= 80){
+            y++; //new line
+            x=0;
+        }
+        if (y >= 25){ //clears the screen if y exceeds the length
+            for(y=0; y < 25; y++){//this loop goes through everything and sets it to empty space char
+                for(x=0; x<80; x++){
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+
+                }
+                x = 0;
+                y = 0;
+            }
+
+        }
 
     }
 
@@ -23,7 +51,8 @@ extern "C" void callConstructors(){ //called in loader.s
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t)
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
+    printf("I love you, cold unfeeling robot arm!");
 
     GlobalDescriptorTable gdt;
 
